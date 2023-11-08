946b5814625ec8d865204c0b010856b9af6b4041
Jade Stewart <jadekstewart3@gmail.com>
2023-11-08 13:41:33 -0700

    add notes to help with understanding

--- lib/diamonds.rb
+++ lib/diamonds.rb
@@ -1,12 +1,19 @@
 def diamonds(n)
   return nil if n.negative? || n.even?
+  #knock out disqualifiers
   diamond = []
+  #create structure to hold strings
   (1..n).step(2) do |stars|
+    #iterate over odd numbers
     spaces = (n - stars) / 2
+    #calculate spaces
     line = " " * spaces + "*" * stars
+    #calculate spaces and number of stars
     diamond << line
+    #shove line into array
   end
     bottom_half = diamond[0...-1].reverse
     diamond.concat(bottom_half)
     diamond.join("\n") + "\n"
+    #reverse add and join
 end
