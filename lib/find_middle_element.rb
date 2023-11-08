7f7d9225949e0e953235ff7f3ddf1f77a0854180
Jade Stewart <jadekstewart3@gmail.com>
2023-11-08 11:16:24 -0700

    add additional solution and take notes on time complexity. my solution was fater :P

--- lib/find_middle_element.rb
+++ lib/find_middle_element.rb
@@ -1,5 +1,14 @@
+#my attempt
+#O(n) because it does not use additional data structures that grow with input
 def middle_element_finder(input_array)
   input_array.each_with_index do |num, i|
     return i if num != input_array.max && num != input_array.min
   end
+end
+
+#not bad
+#forgot that i could call .index tho
+#less effiecient O(n* log(n)) because of sort
+def middle_element_finder(input_array)
+  input_array.index(input_array.sort[1])
 end
