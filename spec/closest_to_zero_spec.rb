c0a9b6a945e79836095803c3942c538c8700e8b5
Jade Stewart <jadekstewart3@gmail.com>
2023-11-17 14:46:33 -0700

    Tests

---
+++ spec/closest_to_zero_spec.rb
@@ -0,0 +1,15 @@
+require "pry"
+require "./lib/closest_to_zero"
+
+RSpec.describe "closest integer to zero" do 
+  it "returns the integer that is closest in proximity to zero" do 
+    expect(closest([10, 3, 9, 1])).to eq(1)
+    expect(closest([2, 4, -1, -3])).to eq(-1)
+    expect(closest([13, 0, -6])).to eq(0)
+  end
+
+  it "returns nil if there is more than one value with the same proximity" do 
+    expect(closest([5, 1, -1, 2, -10])).to eq(nil)
+    expect(closest([5, 11, 11, 2, -1, 1])).to eq(nil)
+  end
+end
