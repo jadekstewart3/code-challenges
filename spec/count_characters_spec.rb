4a1a85d008c75403f9ba80ceee76ec26b84173bc
Jade Stewart <jadekstewart3@gmail.com>
2023-12-29 13:53:15 -0700

    Additional test for count chars

--- spec/count_characters_spec.rb
+++ spec/count_characters_spec.rb
@@ -5,5 +5,6 @@ require "./lib/count_characters"
 RSpec.describe "Count characters in a string" do 
   it "tallys the number of times a character is present in a string" do 
     expect(count_characters("aba")).to eq({"a" => 2, "b" => 1})
+    expect(count_characters("")).to eq({})
   end
 end
