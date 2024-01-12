7f654c7137bc159b8f52ba5ee3c143f5f6c5d763
Jade Stewart <jadekstewart3@gmail.com>
2024-01-12 11:38:57 -0700

    First passing test for make array consecutive

---
+++ spec/make_array_consecutive_spec.rb
@@ -0,0 +1,9 @@
+require "rspec"
+require "pry"
+require "./lib/make_array_consecutive"
+
+RSpec.describe "Make array consecutive" do 
+  it "returns the number of elements needed to make the array consecutive" do 
+    expect(solution([6, 2, 3, 8])).to eq(3)
+  end
+end
