31a5164b9f054143bc68890d2cb28f00d234a9f1
Jade Stewart <jadekstewart3@gmail.com>
2024-01-15 13:52:57 -0700

    Tests

--- spec/make_array_consecutive_spec.rb
+++ spec/make_array_consecutive_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe "Make array consecutive" do
   it "returns the number of elements needed to make the array consecutive" do 
     expect(solution([6, 2, 3, 8])).to eq(3)
   end
+
+  it "returns zero if the elements are already consecutive" do
+    expect(solution([5, 4, 6])).to eq(0)
+  end
 end
