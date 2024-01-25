efe11fe24587c5400440bcc4d18c1bac251693d2
Jade Stewart <jadekstewart3@gmail.com>
2024-01-25 11:23:37 -0700

    Add more test to prove functionality

--- spec/count_salutes_spec.rb
+++ spec/count_salutes_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe "Salute counting" do
   it "returns zero if no one is in the hallway" do 
     expect(count_salutes('------')).to eq(0)
   end
+
+  it "works with many people traveling from the left" do 
+    expect(count_salutes('>>>>>>>>>>>>>>>>>>>>>----<->')).to eq(42)
+  end
+
+  it "counts salutes with a strange meeting pattern" do 
+    expect(count_salutes('<<----<>---<')).to eq(2)
+  end
 end
