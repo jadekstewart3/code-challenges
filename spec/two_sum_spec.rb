b0aed27a3d032003805c534fea8b3dcec9113fdb
Jade Stewart <jadekstewart3@gmail.com>
2024-01-30 16:08:42 -0700

    Add more tests

--- spec/two_sum_spec.rb
+++ spec/two_sum_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe "two sum problem" do
     expect(two_sum([1, 2, 3], 4)).to eq([0,2])
   end
 
-  
+  it "does it with large numbers as well" do 
+    expect(two_sum([1234, 5678, 9012], 14690)).to eq([1, 2])
+  end
+
+  it "works with repeating elements" do 
+    expect(two_sum([2, 2, 3], 4)).to eq([0, 1])
+  end
 end
