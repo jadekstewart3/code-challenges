b0f79e648508da4ce6c37f59df210e4761e223fa
Jade Stewart <jadekstewart3@gmail.com>
2024-01-30 15:16:53 -0700

    Add case sensitive test

--- spec/rot13_spec.rb
+++ spec/rot13_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe "rot 13 cipher" do
   it "decodes lower case text" do 
     expect(rot_13("grfg")).to eq("test")
   end
+
+  it "decodes case-sensitive text" do 
+    expect(rot_13("Grfg")).to eq("Test")
+  end
 end
