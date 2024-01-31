ee4630104b213ac9c36382e8256ada8c8e58838f
Jade Stewart <jadekstewart3@gmail.com>
2024-01-31 12:46:02 -0700

    alternate solution

--- lib/two_sum.rb
+++ lib/two_sum.rb
@@ -1,9 +1,15 @@
+# def two_sum(numbers, target)
+#  num_indecies = Hash.new
+#   numbers.each_with_index do |num, i|
+#     difference = target - num
+#     return [num_indecies[difference] , i] if num_indecies.key?(difference)
+#     num_indecies[num] = i
+#   end
+#  nil
+# end
+
+#alternate clever solution
 def two_sum(numbers, target)
- num_indecies = Hash.new
-  numbers.each_with_index do |num, i|
-    difference = target - num
-    return [num_indecies[difference] , i] if num_indecies.key?(difference)
-    num_indecies[num] = i
-  end
- nil
+  pair = numbers.combination(2).find{ |(a,b)| a+b == target }
+  [numbers.index(pair[0]), numbers.rindex(pair[1])]
 end
