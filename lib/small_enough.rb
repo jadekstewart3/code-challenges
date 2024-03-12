be84d97f46f6646bbdc7bd5890ed5bd1b23d46bf
Jade Stewart <jadekstewart3@gmail.com>
2024-03-12 14:12:39 -0600

    Add refactored solution

--- lib/small_enough.rb
+++ lib/small_enough.rb
@@ -1,6 +1,11 @@
-def small_enough(array, limit)
-  array.each do |num|
-    return false if num > limit
-  end
-  true
+# def small_enough(array, limit)
+#   array.each do |num|
+#     return false if num > limit
+#   end
+#   true
+# end
+
+# refactored solution
+def small_enough(a, limit)
+  a.all? { |num| num <= limit }
 end
