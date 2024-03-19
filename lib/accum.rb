c9dabfa5c22fb289c6ecc18accc748650360c9ed
Jade Stewart <jadekstewart3@gmail.com>
2024-03-18 18:35:34 -0600

    Add lil note for myself

--- lib/accum.rb
+++ lib/accum.rb
@@ -2,5 +2,8 @@ def accum(s)
 	characters = s.chars
   characters.each_with_index.map do |char, index|
     (char * (index + 1)).capitalize
+    require 'pry'; binding.pry
   end.join("-")
 end
+
+## you can multiply strings!
